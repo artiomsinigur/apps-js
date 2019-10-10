8a6a174d8f5528f76c579f002a9fce68e14e5922
ArtiomS <sinigur2006@gmail.com>
2019-10-10 19:02:29 -0400

    Move head of snake with arrows

--- snake-game/snake.js
+++ snake-game/snake.js
@@ -20,14 +20,9 @@ imgGround.src = "img/ground.png";
 let audioName = new Audio();
 audioName.src = "audio/audio.mp3";
 
-// Load gound image
-// ctx.drawImage(imgName, x, y, width, height);
-imgGround.addEventListener("load", () => ctx.drawImage(imgGround, 0, 0));
-
 // Move snake
 let snake = [];
     snake[0] = {x: 9*box, y: 10*box};
-    snake[1] = {x: 8*box, y: 10*box};
 
 // Place food randomly
 let food = {
@@ -37,23 +32,78 @@ let food = {
 let score = 0;
 
 
+// Controle the snake
+let d;
+document.addEventListener("keydown", direction);
+function direction( e ) {
+    if (e.keyCode == 37 && d != "RIGHT") {
+        d = "LEFT";
+    } else if(e.keyCode == 38 && d != "DOWN") {
+        d = "UP";
+    } else if(e.keyCode == 39 && d != "LEFT") {
+        d = "RIGHT";
+    } else if(e.keyCode == 40 && d != "UP") {
+        d = "DOWN";
+    }
+}
+
+
 /**
  * Draw the snake
  */
 function draw() {
+    // Load gound image
+    // ctx.drawImage(imgName, x, y, width, height);
+    ctx.drawImage(imgGround, 0, 0);
+    
     for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = (i == 0) ? "green" : "white";
+        ctx.fillStyle = (i == 0) ? "green" : "#82BB4A"; // Where green are the head. Only first index pos
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
 
-        ctx.strokeStyle = "red";
+        ctx.strokeStyle = "#578A34";
         ctx.strokeRect(snake[i].x, snake[i].y, box, box);
     }
     ctx.drawImage(imgFood, food.x, food.y);
 
+    // Write score
     ctx.fillStyle = "white";
     ctx.font = "34px Arial";
     ctx.fillText(score, 2*box, 1.6*box);
-}
-let game = setInterval(draw, 1000);
 
+
+    // General rules. How snake move.
+        // 1. Remove tail (snake.pop())
+        // 2. Add new head (snake.unshift(3))
+        // 3. Repeat
+
+    // Get old head postion
+    let snakeHeadX = snake[0].x;
+    let snakeHeadY = snake[0].y;
+    
+
+    // Check the direction
+    // If snake moves on the left 
+    if (d == "LEFT") snakeHeadX -= box; 
+    // If snake moves on the right
+    if (d == "RIGHT") snakeHeadX += box;
+    // If snake moves on the top 
+    if (d == "UP") snakeHeadY -= box;
+    // If snake moves on the bottom 
+    if (d == "DOWN") snakeHeadY += box;
+
+
+    // Delete snake tail
+    snake.pop();
+
+
+
+    // Add new head
+    let newHead = {
+        x:snakeHeadX,
+        y:snakeHeadY
+    };
+
+    snake.unshift(newHead);
+}
+let game = setInterval(draw, 100);
 
